8a212eeb125ad1de173d92a2de31438f76409f55
sidyjames <95179072+sidyjames@users.noreply.github.com>
2023-12-04 16:50:26 +0100

    login and homepage maj

--- app/src/main/java/com/example/h3messenger/MainActivity.kt
+++ app/src/main/java/com/example/h3messenger/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.h3messenger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,19 +14,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.example.h3messenger.ui.theme.H3MessengerTheme
-import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("h3 messenger" , modifier=Modifier.padding(70.dp,200.dp,50.dp,100.dp))
+                   Navigation()
                 }
             }
         }
@@ -49,14 +43,13 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String,modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier, onsubmit: ()-> Unit) {
     var it by remember { mutableStateOf("") }
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "Bienvenue sur $name !",
+            text = "Bienvenue !",
             modifier = Modifier.padding(bottom = 4.dp)
         )
         OutlinedTextField(
@@ -67,7 +60,7 @@ fun Greeting(name: String,modifier: Modifier = Modifier) {
                 .fillMaxWidth()
                 .padding(bottom = 8.dp)
         )
-        Button(onClick = {  }) {
+        Button(onClick = onsubmit) {
             Text("Y ACCEDER")
         }
     }
@@ -75,10 +68,11 @@ fun Greeting(name: String,modifier: Modifier = Modifier) {
 
 @Composable
 fun Navigation(){
-    val currentPage = remember { mutableStateOf("") }
+    val currentPage = remember { mutableStateOf("login") }
     when (currentPage.value){
-        "login" -> GreetingPreview()
+        "login" -> Greeting {currentPage.value= "home"}
         "home" -> HomePage()
+        else -> Text(text = "404 not found")
 
     }
 }
@@ -93,19 +87,12 @@ fun HomePage(modifier: Modifier = Modifier){
         modifier = modifier
     ) {
         Text(
-            text = "Bienvenue !",
+            text = "Bienvenue sur le HomePage !",
             modifier = Modifier.padding(bottom = 4.dp)
         )
-        OutlinedTextField(
-            value = it,
-            onValueChange = { newName -> it = newName },
-            label = { Text("Entrez votre nom") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        )
+
         Button(onClick = {  }) {
-            Text("")
+            Text("HomePage")
         }
     }
 }
@@ -113,6 +100,6 @@ fun HomePage(modifier: Modifier = Modifier){
 @Composable
 fun GreetingPreview() {
     H3MessengerTheme {
-        if (true) Greeting("Android") else HomePage()
+        Navigation()
     }
 }
